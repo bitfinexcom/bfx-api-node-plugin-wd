b0d003b964553ebb5e5b5469f5dee119a9d3c3a9
Cris Mihalache <me@f3rno.com>
2019-10-04 21:22:17 +0700

    (feature) normalize docs, bump to 1.0.2

--- example/usage.js
+++ example/usage.js
@@ -4,7 +4,7 @@ process.env.DEBUG = '*'
 
 const debug = require('debug')('bfx:api:plugins:wd:example')
 const { Manager } = require('bfx-api-node-core')
-const WDPlugin = require('../')
+const WDPlugin = require('..')
 
 const mgr = new Manager({
   transform: true,
